0ba9fd7cee36d458ffd536baa606a788cf4179bd
Bruno Botelho <bruno.botelho.br@gmail.com>
2025-05-19 17:41:35 -0300

    Remove copyright notices and license information from multiple PHP files

--- php/example1/wp-config.php
+++ php/example1/wp-config.php
@@ -1,18 +1,4 @@
 <?php
-// Copyright 2021 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 /**
  *  * The base configuration for WordPress
  *   *
@@ -79,18 +65,7 @@ define( 'NONCE_SALT',       'put your unique phrase here' );
  */
 $table_prefix = 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
+
 define( 'WP_DEBUG', false );
 
 /* That's all, stop editing! Happy publishing. */
